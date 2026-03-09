8e50e3ffb305f08c38df7fc4691552ce51b9e9c3
Mohamed Moustafa Ahmed <119596488+Mohamed-Moustafa-115@users.noreply.github.com>
2026-03-09 15:15:03 +0200

    Modify install-Flutter.sh for JDK and Android Studio
    Updated script to install JDK 17.0.12 and Android Studio. Commented out VS Code and Android Studio installation sections.

--- install-Flutter.sh
+++ install-Flutter.sh
@@ -1,17 +1,32 @@
+#Get user's home directory
+read -p  "👤 Current home username: "    USER_NAME
+cd /home/$USER_NAME
+mkdir "Flutter IDE"
+
 #Install required packages
 echo "Installing required packages"
-sudo apt-get update -y && sudo apt-get upgrade -y
+#sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 
 #Install FlutterSDK
 echo "Installing FlutterSDK"
 git clone -b main https://github.com/flutter/flutter.git
 ./flutter/bin/flutter --version
+echo export PATH="$PATH:/home/$USER_NAME/flutter/bin" >> .bashrc
+source .bashrc
+flutter doctor
 
-#Install Android studio
-echo "Installing Android studio"
+#Download and install JDK 17.0.12
+cd Flutter\ IDE/
+echo "Downloading JDK 17.0.12"
+curl -# -O https://download.oracle.com/java/17/archive/jdk-17.0.12_linux-x64_bin.deb
+echo "Installing JDK 17.0.12 (Enter your sudo password)"
+sudo apt install ./jdk-17.0.12_linux-x64_bin.deb -y
+
+#Download Android studio
+echo "Downloading Android studio"
 curl -# -O https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.2.6/android-studio-panda2-linux.tar.gz
 
-#Install VS Code
-echo "Installing VS Code"
+#Download VS Code
+echo "Downloading VS Code"
 curl -# -O https://vscode.download.prss.microsoft.com/dbazure/download/stable/61b3d0ab13be7dda2389f1d3e60a119c7f660cc3/code_1.110.1-1772839366_amd64.deb
