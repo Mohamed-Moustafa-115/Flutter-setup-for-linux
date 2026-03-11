ff6496abc48952c2c12268016b5e6c188f6dc204
Mohamed Moustafa Ahmed <119596488+Mohamed-Moustafa-115@users.noreply.github.com>
2026-03-11 17:32:13 +0200

    Update install-Flutter.sh

--- install-Flutter.sh
+++ install-Flutter.sh
@@ -6,7 +6,7 @@ mkdir "Flutter IDE"
 #Install required packages
 echo "Installing required packages"
 #sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+sudo apt-get install -y curl unzip xz-utils zip libglu1-mesa
 
 #Install FlutterSDK
 echo "Installing FlutterSDK"
@@ -24,10 +24,12 @@ ls -l
 echo "Installing JDK 17.0.12 (Enter your sudo password)"
 sudo apt install ./jdk-17.0.12_linux-x64_bin.deb -y
 
-#Download Android studio
-echo "Downloading Android studio"
-curl -# -O https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.2.6/android-studio-panda2-linux.tar.gz
-
 #Download VS Code
 echo "Downloading VS Code"
 curl -# -O https://vscode.download.prss.microsoft.com/dbazure/download/stable/61b3d0ab13be7dda2389f1d3e60a119c7f660cc3/code_1.110.1-1772839366_amd64.deb
+echo "Installing VS Code"
+sudo apt install ./code_1.110.1-1772839366_amd64.deb -y
+
+#Download Android studio
+echo "Downloading Android studio"
+curl -# -O https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.2.6/android-studio-panda2-linux.tar.gz
