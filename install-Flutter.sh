3a6d8208c508a4e9a2d1db739cec540d63e16573
Mohamed Moustafa Ahmed <119596488+Mohamed-Moustafa-115@users.noreply.github.com>
2026-03-18 06:40:04 +0200

    Added change directory after moving Android Studio

--- install-Flutter.sh
+++ install-Flutter.sh
@@ -36,6 +36,7 @@ echo "Downloading Android studio"
 curl -# -O https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.2.6/android-studio-panda2-linux.tar.gz
 tar -xzvf 'android-studio-panda2-linux.tar.gz' --checkpoint=.1000
 mv "android-studio" ../
+cd ../
 
 while true; do
     # For GitHub Actions, use an environment variable
