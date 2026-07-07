350fa9dabd59bb065bc1035638975fd683fcba3a
Mohamed Moustafa Ahmed <119596488+Mohamed-Moustafa-115@users.noreply.github.com>
2026-07-07 15:14:14 +0300

    Enhance echo statements with color formatting

--- install-Flutter.sh
+++ install-Flutter.sh
@@ -1,16 +1,16 @@
 #Get user's home directory
 USER_NAME=$(whoami)
 cd /home/$USER_NAME
-echo "Creating a directory for Flutter requirements"
+echo "\e[46mCreating a directory for Flutter requirements\e[0m"
 mkdir "Flutter Requirements"
 
 #Install required packages
-echo "Updating and installing required packages"
+echo "\e[46mUpdating and installing required packages\e[0m"
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y curl unzip xz-utils zip libglu1-mesa
 
 #Install FlutterSDK
-echo "Installing FlutterSDK"
+echo "\e[46mInstalling FlutterSDK\e[0m"
 git clone -b main https://github.com/flutter/flutter.git
 ./flutter/bin/flutter --version
 echo export PATH="$PATH:/home/$USER_NAME/flutter/bin" >> .bashrc
@@ -19,20 +19,20 @@ flutter doctor
 
 #Download and install JDK 17.0.12
 cd "Flutter Requirements"
-echo "Downloading JDK 17.0.12"
+echo "\e[46mDownloading JDK 17.0.12\e[0m"
 curl -# -O https://download.oracle.com/java/17/archive/jdk-17.0.12_linux-x64_bin.deb
 ls -l
-echo "Installing JDK 17.0.12 (Enter your sudo password)"
+echo "\e[46mInstalling JDK 17.0.12 (Enter your sudo password)\e[0m"
 sudo apt install ./jdk-17.0.12_linux-x64_bin.deb -y
 
 #Download VS Code
-echo "Downloading VS Code"
+echo "\e[46mDownloading VS Code\e[0m"
 curl -# -O https://vscode.download.prss.microsoft.com/dbazure/download/stable/6928394f91b684055b873eecb8bc281365131f1c/code_1.124.2-1781225536_amd64.deb
-echo "Installing VS Code"
+echo "\e[46mInstalling VS Code\e[0m"
 sudo apt install ./code_1.124.2-1781225536_amd64.deb -y
 
 #Download and install Android studio
-echo "Downloading Android studio"
+echo "\e[46mDownloading Android studio\e[0m"
 curl -# -O https://edgedl.me.gvt1.com/android/studio/ide-zips/2026.1.1.9/android-studio-quail1-patch1-linux.tar.gz
 tar -xzvf 'android-studio-quail1-patch1-linux.tar.gz' --checkpoint=.1000
 mv "android-studio" ../
@@ -46,17 +46,17 @@ while true; do
       echo "GitHub Actions detected, using: $CHOICE"
   else
       # Interactive terminal
-      read -p "Do you want to keep Flutter Requirements folder (Setup packages) y/n " CHOICE
+      read -p "\e[46mDo you want to keep Flutter Requirements folder (Setup packages) y/n\e[0m" CHOICE
   fi
   if [ "$CHOICE" = "n" ] || [ "$CHOICE" = "N" ]; then
     sudo rm -rf "Flutter Requirements"
-    echo "Flutter Requirements folder is removed"
-    echo "Setup finsihed, now run Andriod Studio to download the remaining SDK components"
+    echo "\e[46mFlutter Requirements folder is removed\e[0m"
+    echo "\e[46mSetup finsihed, now run Andriod Studio to download the remaining SDK components\e[0m"
     break
   elif [ "$CHOICE" = "y" ] || [ "$CHOICE" = "Y" ]; then
-    echo "Setup finsihed, now run Android Studio to download the remaining SDK components"
+    echo "\e[46mSetup finsihed, now run Android Studio to download the remaining SDK components\e[0m"
     break
   else
-  echo "Invalid input"
+  echo "\e[41mInvalid input\e[0m"
   fi
 done
